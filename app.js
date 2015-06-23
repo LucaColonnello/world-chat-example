eda28d0cc353b94e1ffbf2f5266703d34798d113
Luca Colonnello <luca.colonnello91@gmail.com>
2015-06-23 20:44:28 +0200

    Test from mongose

--- app.js
+++ app.js
@@ -1,51 +1,11 @@
-//
-// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
-// Mongoose is a object/data mapping utility for the MongoDB database.
-//
+var http = require('http');
+var mongoose = require("mongoose");
 
-// by Ben Wen with thanks to Aaron Heckmann
-
-//
-// Copyright 2014 ObjectLabs Corp.  
-// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
-//
-// MIT Licensed
-//
-
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation files
-// (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of the Software,
-// and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:  
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software. 
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE. 
-
-//
-// Preamble
-var http = require ('http');	     // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
-
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.  
 var uristring = 
   process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
   'mongodb://localhost';
 
-// The http server will listen to an appropriate port, or default to
-// port 5000.
 var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
@@ -58,50 +18,33 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
-// This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var userSchema = new mongoose.Schema({
+// mnessage schema
+var messageSchema = new mongoose.Schema({
   name: {
-    first: String,
-    last: { type: String, trim: true }
-  },
-  age: { type: Number, min: 0}
+    username: String,
+    message: { type: String, trim: true }
+  }
 });
 
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var PUser = mongoose.model('PowerUsers', userSchema);
+var Message = mongoose.model('Messages', messageSchema);
 
+/*
 // Clear out old data
 PUser.remove({}, function(err) {
   if (err) {
     console.log ('error deleting old data.');
   }
 });
+*/
 
-// Creating one user.
-var johndoe = new PUser ({
-  name: { first: 'John', last: '  Doe   ' },
-  age: 25
+// create one message
+var newMessage = new Message({
+  username: 'John Doe',
+  message: 'Ciao come va? !"£$%&/()=?è*ç°§.-'
 });
 
-// Saving it to the database.  
-johndoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var janedoe = new PUser ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var alicesmith = new PUser ({
-  name: { first: 'Alice', last: 'Smith' },
-  age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
-
+// Saving it to the database.
+newMessage.save(function (err) {if (err) console.log ('Error on save!')});
 
 // In case the browser connects before the database is connected, the
 // user will see this message.
@@ -118,18 +61,17 @@ http.createServer(function (req, res) {
 
 function createWebpage (req, res) {
   // Let's find all the documents
-  PUser.find({}).exec(function(err, result) { 
+  Message.find({}).exec(function(err, result) { 
     if (!err) { 
       res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
       // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
-      var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
-      query.where('age').gt(64);
+      var query = Message.find( { username: /doe/i } ); // (ok in this example, it's all entries)
       query.exec(function(err, result) {
-	if (!err) {
-	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
-	} else {
-	  res.end('Error in second query. ' + err)
-	}
+      	if (!err) {
+      	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
+      	} else {
+      	  res.end('Error in second query. ' + err)
+      	}
       });
     } else {
       res.end('Error in first query. ' + err)
