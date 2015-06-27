8e0c76556ceeb98175be241f331384aadb0296b9
Aletv <aletv@live.it>
2015-06-27 17:21:49 +0200

    ambiente client

--- client/script.js
+++ client/script.js
@@ -1,37 +1,89 @@
 // declare a module
-var app = angular.module('myApp', []);
+var app = angular.module('worldChat', ["ngRoute"]);
 
-app.controller('FirstController', function($scope){
-	$scope.hello = "Hello!!";
-	$scope.name ="Alessio";
-	$scope.lista=[1,2];
-	
-	$scope.newUser = {
-		name: "",
-		eta: 0
-	};
-	
-	$scope.listaOgg=[
-		{
-			name:"luca",
-			eta:24
-		},
-		{
-			name:"Alessio",
-			eta:24
+app.config( function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      }).
+      when('/chat', {
+        templateUrl: 'views/chat.html',
+        controller: 'ChatCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+} );
+
+app.factory('user', function () {
+  return {
+	username:""
+  };
+});
+
+//factory socket chat
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
+
+app.controller("HomeCtrl",function($scope,$location,user){
+	$scope.user=user;
+	$scope.submit=function(){
+		if($scope.user.username==""){
+			alert("E' necessario inserire un nome utente per accedere alla chat.");		
+			return;
 		}
-		
-	];
-	$scope.count = function(){
-		return $scope.listaOgg.length;
+			
+		$location.path("/chat");
 	};
+});
+
+app.controller("ChatCtrl",function($scope,$location,socket,user){
+	$scope.user = user;
 	
-	$scope.addUser = function(){
-		$scope.listaOgg.push( Object.create( $scope.newUser ) );
-		$scope.newUser = {
-			name: "",
-			eta: 0
+	if($scope.user.username == ""){
+		$location.path("/");		
+		return;
+	}
+	
+	$scope.newMessage = {
+		username: $scope.user.username,
+		message: ''
+	};
+	$scope.messages = [ ];
+	
+	
+	socket.on( "newMessageSent", function( data ) {
+		$scope.messages.push( data );
+	} );
+	
+	$scope.submit = function( ){
+		socket.emit( "postMessage", $scope.newMessage );
+		$scope.newMessage = {
+			username: $scope.user.username,
+			message: ''
 		};
 	};
-
+	
 });
